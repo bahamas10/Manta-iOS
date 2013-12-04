493616a5813e3534002a2b2abfea89ecac82fe66
Dave Eddy <dave@daveeddy.com>
2013-12-04 15:02:55 -0500

    properly download files, create local filesystem representation

--- Manta/FilesTableViewController.m
+++ Manta/FilesTableViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "FilesTableViewController.h"
-#import <AFNetworking/AFHTTPRequestOperationManager.h>
 #import "JSONStreamResponseSerializer.h"
+
+#import <CommonCrypto/CommonCrypto.h>
+
+#import <AFNetworking/AFHTTPRequestOperationManager.h>
+#import <AFNetworking/AFURLSessionManager.h>
 
 @interface FilesTableViewController ()
 
@@ -27,11 +31,19 @@
               @"url": @"https://us-east.manta.joyent.com"
               },
           @{
+              @"name": @"papertigers",
+              @"url": @"https://us-east.manta.joyent.com"
+              },
+          @{
               @"name": @"Joyent_Dev",
               @"url": @"https://us-east.manta.joyent.com"
               },
           @{
               @"name": @"devops@voxer.com",
+              @"url": @"https://us-east.manta.joyent.com"
+              },
+          @{
+              @"name": @"bahamas11-fake",
               @"url": @"https://us-east.manta.joyent.com"
               }
           ];
@@ -44,12 +56,8 @@
     [super viewDidLoad];
     [self.tableView registerClass:UITableViewCell.class forCellReuseIdentifier:@"UserCell"];
     
-    // conditional edit button
-    NSArray *components = [self.currentPath componentsSeparatedByString:@"/"];
-    
-    if ((self.isRootView) ||
-        (self.getLevel >= 2 && components.count >= 3 && ![components[2] isEqualToString:@"public"]))
-        self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    if (self.canEdit)
+        self.navigationItem.leftBarButtonItem = self.editButtonItem;
 
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
@@ -77,23 +85,76 @@
     manager.responseSerializer = [JSONStreamResponseSerializer serializer];
     [manager GET:self.currentPath parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
         UIApplication.sharedApplication.networkActivityIndicatorVisible = NO;
-        self.files = responseObject;
+        NSArray *sortedArray;
+        // sort the files
+        sortedArray = [responseObject sortedArrayUsingComparator:^NSComparisonResult(id a, id b) {
+            if ([a[@"type"] isEqualToString:@"directory"] && ![b[@"type"] isEqualToString:@"directory"])
+                return NSOrderedAscending;
+            if ([b[@"type"] isEqualToString:@"directory"] && ![a[@"type"] isEqualToString:@"directory"])
+                return NSOrderedDescending;
+            return [a[@"name"] compare:b[@"name"]];
+        }];
+        self.files = [NSMutableArray arrayWithArray:sortedArray];
         
         [self.tableView reloadData];
         [self.refreshControl endRefreshing];
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         UIApplication.sharedApplication.networkActivityIndicatorVisible = NO;
         
-        // if these conditions are true, we are accesing something we only have public access to
-        if (operation.response.statusCode == 403 && self.getLevel == 1)
+        if (operation.response.statusCode == 403 && self.getLevel == 1) {
+            // if these conditions are true, we are accesing something we only have public access to
             self.files = [NSMutableArray arrayWithArray:@[@{@"name": @"public", @"type": @"directory"}]];
+        } else {
+            // a real error occurred, make an alert
+            NSString *msg =  error.userInfo[NSLocalizedDescriptionKey];
+            if (!msg)
+                msg = @"An unknown error has occurred";
+            UIAlertView *alert = [[UIAlertView alloc]
+                                  initWithTitle:@"Error"
+                                  message:msg
+                                  delegate:nil
+                                  cancelButtonTitle:@"Dismiss"
+                                  otherButtonTitles:nil];
+            
+            [alert show];
+        }
         
         [self.tableView reloadData];
         [self.refreshControl endRefreshing];
     }];
 }
 
+- (void)downloadFile:(NSString *)filename
+{
+    NSURL *URL = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@/%@",
+                                       self.mantaURL.absoluteString,
+                                       self.currentPath,
+                                       filename]];
+    NSString *localFile = [[self documentsDirectoryPath] stringByAppendingPathComponent:URL.path];
+    NSLog(@"localFile = %@", localFile);
+    
+    BOOL isDir = NO;
+    BOOL fileExists = [NSFileManager.defaultManager fileExistsAtPath:localFile isDirectory:&isDir];
+    if (fileExists && isDir) {
+        // delete the directory, because the remote end says it is a file (object)
+        NSLog(@"%@ is a directory when it should be a file, removing it", localFile);
+        [NSFileManager.defaultManager removeItemAtPath:localFile error:nil];
+    }
+    
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+    
+    NSURLSessionDownloadTask *downloadTask = [manager downloadTaskWithRequest:request progress:nil destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {
+        return [NSURL fileURLWithPath:localFile];
+    } completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {
+        NSLog(@"File downloaded to: %@", filePath);
+    }];
+    [downloadTask resume];
+}
+
 #pragma mark - IBAction
+
 - (IBAction)refresh:(id)sender
 {
     [self refresh];
@@ -116,7 +177,6 @@
     static NSString *CellIdentifier = @"UserCell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
-    // Configure the cell...
     NSDictionary *file = self.files[indexPath.row];
     cell.textLabel.text = file[@"name"];
     
@@ -125,8 +185,9 @@
 
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return YES;
+    return self.canEdit;
 }
+
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
@@ -165,13 +226,10 @@
 
     NSDictionary *object = self.files[indexPath.row];
     if ([object[@"type"] isEqualToString:@"object"]) {
-        NSString *urlString = [NSString stringWithFormat:@"%@%@/%@",
-                               self.mantaURL.absoluteString,
-                               self.currentPath,
-                               object[@"name"]];
-        NSLog(@"urlString = %@", urlString);
-        [UIApplication.sharedApplication openURL:[NSURL URLWithString:urlString]];
+        // file click
+        [self downloadFile:object[@"name"]];
     } else {
+        // sub-directory click
         FilesTableViewController *newSubdirectoryController = [[FilesTableViewController alloc] init];
         NSString *subpath = [self.currentPath stringByAppendingPathComponent:object[@"name"]];
         newSubdirectoryController.currentPath = subpath;
@@ -184,6 +242,22 @@
         //newSubdirectoryController.tableCellID = self.tableCellID;
         
         //[newSubdirectoryController listDirectoryAtPath:subpath];
+        
+        // make the sub-directory if necessary
+        NSString *localSubDirectory = [[self documentsDirectoryPath] stringByAppendingPathComponent:subpath];
+        NSLog(@"localSubDirectory = %@", localSubDirectory);
+        BOOL isDir = NO;
+        BOOL fileExists = [NSFileManager.defaultManager fileExistsAtPath:localSubDirectory isDirectory:&isDir];
+        if (!fileExists) {
+            // create the dir
+            [NSFileManager.defaultManager createDirectoryAtPath:localSubDirectory withIntermediateDirectories:NO attributes:nil error:nil];
+        } else if (fileExists && !isDir) {
+            // delete the file, then create the dir
+            [NSFileManager.defaultManager removeItemAtPath:localSubDirectory error:nil];
+            [NSFileManager.defaultManager createDirectoryAtPath:localSubDirectory withIntermediateDirectories:NO attributes:nil error:nil];
+        }
+        
+        
         [tableView deselectRowAtIndexPath:indexPath animated:YES];
         [self.navigationController pushViewController:newSubdirectoryController animated:YES];
     }
@@ -202,13 +276,67 @@
  */
 
 #pragma mark - Helper functions
+// check if the current view is the root of the stack
 - (BOOL)isRootView
 {
     return self.getLevel ? NO : YES;
 }
+// get the current stack level of the views
 - (NSInteger)getLevel
 {
     return self.navigationController.viewControllers.count - 1;
 }
+// determine if the current view should be editable
+- (BOOL)canEdit
+{
+    NSArray *components = [self.currentPath componentsSeparatedByString:@"/"];
+    return (self.isRootView) ||
+           (self.getLevel >= 2 && components.count >= 3 && ![components[2] isEqualToString:@"public"]);
+}
+// return the document directory path
+- (NSString *)documentsDirectoryPath
+{
+    return [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+}
+// return the document directory URL
+- (NSURL *)documentsDirectoryURL
+{
+    return [NSURL fileURLWithPath:[self documentsDirectoryPath]];
+}
+
+#pragma mark - RSA
+- (NSData *)getSignatureBytes:(NSData *)plainText withPrivateKey:(SecKeyRef)privateKey
+{
+	OSStatus sanityCheck = noErr;
+	NSData * signedHash = nil;
+	
+	size_t signedHashBytesSize = SecKeyGetBlockSize(privateKey);
+	uint8_t *signedHashBytes = malloc(signedHashBytesSize * sizeof(uint8_t));
+    if (signedHashBytes == NULL)
+        return nil;
+    
+	memset((void *)signedHashBytes, 0x0, signedHashBytesSize);
+	sanityCheck = SecKeyRawSign(privateKey,
+                                kSecPaddingPKCS1SHA256,
+                                (const uint8_t *)[[self getHash256Bytes:plainText] bytes],
+                                CC_SHA256_DIGEST_LENGTH,
+                                (uint8_t *)signedHashBytes,
+                                &signedHashBytesSize);
+	
+	signedHash = [NSData dataWithBytes:(const void *)signedHashBytes length:(NSUInteger)signedHashBytesSize];
+	if (signedHashBytes)
+        free(signedHashBytes);
+	
+	return signedHash;
+}
+
+- (NSData *)getHash256Bytes:(NSData *)plainText
+{
+    NSMutableData *macOut = [NSMutableData dataWithLength:CC_SHA256_DIGEST_LENGTH];
+    CC_SHA256(plainText.bytes, plainText.length,  macOut.mutableBytes);
+    return macOut;
+}
+
+
 
 @end
